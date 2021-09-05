ab05c377145e7f43a338a6386afe1a8567f2de60
KrishnaKumar <krishna.kumar2@gds.ey.com>
2021-09-05 11:17:30 +0530

    Added footer and styles order alpha

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -20,20 +20,30 @@ const SideBar = () => {
       <div className="nav-bar-scroll">
         <ProfileCard />
         <MenuList menuItems={NAV_ITEMS} />
+        <section className="recently-viewed">
+          <h2 className="title">Recently viewed</h2>
+          <ul className="items">
+            {RECENTLY_VIEWS.map(({ id, name }) => (
+              <li key={id}>
+                <a href="#">
+                  <span>{name}</span>
+                  <BsArrowRight />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <footer className="credits">
+          <div className="credit">
+            <strong>Inspired By: </strong>
+            <span>dribbble.com/janlosert</span>
+          </div>
+          <div className="credit">
+            <strong>Developed By: </strong>
+            <span>KrishnaKumar</span>
+          </div>
+        </footer>
       </div>
-      <section className="recently-viewed">
-        <h2 className="title">Recently viewed</h2>
-        <ul className="items">
-          {RECENTLY_VIEWS.map(({ id, name }) => (
-            <li key={id}>
-              <a href="#">
-                <span>{name}</span>
-                <BsArrowRight />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </section>
     </nav>
   );
 };
