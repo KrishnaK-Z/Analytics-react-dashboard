a46138268cc35ddd2787498948c860ef034c0494
KrishnaKumar <krishna.kumar2@gds.ey.com>
2021-09-05 15:23:52 +0530

    scroll effect

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -4,6 +4,7 @@ import "./loader.scss";
 const Loader = () => {
   return (
     <div className="loader-ring">
+      <h2>Loading...</h2>
       <div className="loader-ring-light" />
     </div>
   );
